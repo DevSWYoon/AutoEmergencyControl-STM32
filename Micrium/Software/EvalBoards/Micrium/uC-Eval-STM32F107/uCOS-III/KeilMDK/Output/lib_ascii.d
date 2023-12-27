0c771386618cbaf0fc7e3d2bc39c8211b1fa7891
DevSWYoon <aicsay4838@gmail.com>
2023-12-28 01:13:57 +0900

    applying gitnore

--- Micrium/Software/EvalBoards/Micrium/uC-Eval-STM32F107/uCOS-III/KeilMDK/Output/lib_ascii.d
+++
@@ -1,5 +0,0 @@
-./output/lib_ascii.o: ..\..\..\..\..\uC-LIB\lib_ascii.c \
-  ..\..\..\..\..\uC-LIB\lib_ascii.h \
-  ..\..\..\..\..\uC-CPU\ARM-Cortex-M3\RealView\cpu.h \
-  ..\..\..\..\..\uC-CPU\cpu_def.h ..\..\uCOS-III\cpu_cfg.h \
-  ..\..\..\..\..\uC-LIB\lib_def.h
--- Micrium/Software/EvalBoards/Micrium/uC-Eval-STM32F107/uCOS-III/KeilMDK/Output/stm32f10x_flash.d
+++
@@ -1,5 +0,0 @@
-./output/stm32f10x_flash.o: ..\..\BSP\ST\STM32\src\stm32f10x_flash.c \
-  ..\..\BSP\ST\STM32\inc\stm32f10x_flash.h \
-  ..\..\BSP\ST\STM32\inc\stm32f10x_map.h ..\..\uCOS-III\stm32f10x_conf.h \
-  ..\..\BSP\ST\STM32\inc\stm32f10x_type.h \
-  ..\..\BSP\ST\STM32\inc\cortexm3_macro.h
